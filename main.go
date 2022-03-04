24f40a7bbab61f01498670291b3dd15a77fbccdb
lufia <lufia@lufia.org>
2022-03-05 01:53:39 +0900

    fix redundant newline

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func usage() {
 	fmt.Fprintf(w, "usage: %s [options] [commands]\n", os.Args[0])
 	flag.PrintDefaults()
 
-	fmt.Fprintln(w, "\navailable commands:\n")
+	fmt.Fprintf(w, "\navailable commands:\n\n")
 	var cmds []string
 	for s := range commands {
 		cmds = append(cmds, s)
