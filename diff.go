0eed203bcb1168ad0354ebbaf724a6dbefa93184
lufia <lufia@lufia.org>
2026-02-21 22:42:01 +0900

    diff: reduce equal lines

--- diff.go
+++ diff.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"path/filepath"
+	"slices"
+	"strings"
 
 	"github.com/sergi/go-diff/diffmatchpatch"
 )
@@ -62,8 +65,150 @@ func diffFiles(w io.Writer, f1, f2 string) error {
 	if err != nil {
 		return err
 	}
+	fmt.Fprintf(w, "--- %s\n", f1)
+	fmt.Fprintf(w, "+++ %s\n", f2)
 	d := diffmatchpatch.New()
-	diffs := d.DiffMain(string(t1), string(t2), true)
-	fmt.Fprintln(w, d.DiffPrettyText(diffs))
+	a, b, c := d.DiffLinesToChars(string(t1), string(t2))
+	diffs := d.DiffCharsToLines(d.DiffMain(a, b, false), c)
+	for h := range reduceDiffs(diffs) {
+		h.WriteTo(w)
+	}
 	return nil
+}
+
+type Hunk struct {
+	// line number at the top of this hunk
+	oldLine, oldLen int
+	newLine, newLen int
+	diffs           []*Diff
+}
+
+func (h *Hunk) WriteTo(w io.Writer) (int64, error) {
+	fmt.Fprintf(w, "@@ -%d,%d +%d,%d @@\n", h.oldLine, h.oldLen, h.newLine, h.newLen)
+	var written int
+	for _, d := range h.diffs {
+		switch d.kind {
+		case -1:
+			n, _ := fmt.Fprintf(w, "-%s\n", d.text)
+			written += n
+		case 0:
+			n, _ := fmt.Fprintf(w, " %s\n", d.text)
+			written += n
+		case 1:
+			n, _ := fmt.Fprintf(w, "+%s\n", d.text)
+			written += n
+		}
+	}
+	return int64(written), nil
+}
+
+type Diff struct {
+	kind int // -1: delete, 1: add, 0: equal
+	text string
+}
+
+type pusher struct {
+	prev  *Hunk
+	yield func(*Hunk) bool
+}
+
+func (p *pusher) Push(h *Hunk) bool {
+	if len(h.diffs) == 0 {
+		return true
+	}
+	rv := true
+	if p.prev != nil && hasModified(p.prev.diffs) {
+		rv = p.yield(p.prev)
+	}
+	p.prev = h
+	return rv
+}
+
+func (p *pusher) Flush() bool {
+	rv := true
+	if p.prev != nil {
+		rv = p.yield(p.prev)
+	}
+	p.prev = nil
+	return rv
+}
+
+func hasModified(diffs []*Diff) bool {
+	return slices.ContainsFunc(diffs, func(d *Diff) bool {
+		return d.kind != 0
+	})
+}
+
+// reduceDiffs snips the middle of diff's text that has type=equal,
+// and it returns sequence of a pair of current and next [Hunk]s.
+func reduceDiffs(diffs []diffmatchpatch.Diff) iter.Seq[*Hunk] {
+	return func(yield func(*Hunk) bool) {
+		push := pusher{yield: yield}
+		var (
+			hunk    = &Hunk{oldLine: 1, newLine: 1}
+			oldNext = 1
+			newNext = 1
+		)
+		for _, d := range diffs {
+			text := d.Text
+			if text[len(text)-1] == '\n' {
+				text = text[:len(text)-1]
+			}
+			a := strings.Split(text, "\n")
+			if len(a) == 0 {
+				continue
+			}
+			switch d.Type {
+			case diffmatchpatch.DiffEqual:
+				before, after := splitLines(a, 3)
+				hunk.diffs = append(hunk.diffs, makeDiffs(before, 0)...)
+				hunk.oldLen += len(before)
+				hunk.newLen += len(before)
+				if !push.Push(hunk) {
+					return
+				}
+				hunk = &Hunk{
+					oldLine: oldNext + len(a) - len(after),
+					oldLen:  len(after),
+					newLine: newNext + len(a) - len(after),
+					newLen:  len(after),
+					diffs:   makeDiffs(after, 0),
+				}
+				oldNext += len(a)
+				newNext += len(a)
+			case diffmatchpatch.DiffInsert:
+				hunk.diffs = append(hunk.diffs, makeDiffs(a, 1)...)
+				hunk.newLen += len(a)
+				newNext += len(a)
+			case diffmatchpatch.DiffDelete:
+				hunk.diffs = append(hunk.diffs, makeDiffs(a, -1)...)
+				hunk.oldLen += len(a)
+				oldNext += len(a)
+			default:
+				panic("unknown type: " + d.Type.String())
+			}
+		}
+		push.Push(hunk)
+		push.Flush()
+	}
+}
+
+func splitLines(a []string, n int) (before, after []string) {
+	before = a
+	if len(before) > n {
+		before = before[:n]
+	}
+	after = a[len(before):]
+	if len(after) > n {
+		after = after[len(after)-n:]
+	}
+	return
+}
+
+func makeDiffs(a []string, kind int) []*Diff {
+	diffs := make([]*Diff, len(a))
+	for i, s := range a {
+		diffs[i] = &Diff{kind: kind, text: s}
+	}
+	return diffs
 }
